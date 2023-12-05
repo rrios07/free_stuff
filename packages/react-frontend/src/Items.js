b09f7d66c1293b8c180b81d9ba9a71bdaef3fc4b
TheLuckyPerson <tylerle122002@gmail.com>
2023-12-05 01:51:48 -0800

    added item icon display

--- packages/react-frontend/src/Items.js
+++ packages/react-frontend/src/Items.js
@@ -6,29 +6,25 @@ function Item(props)
         <Link to='a'>
             <div
             style={{
-                    padding: '2px',
                     color: 'black',
-                    width: '250px',
-                    height: '250px',
-                    gap: '20px',
-                    boxShadow: '0px 0px 3px black'
+                    width: '1165px',
+                    height: '200px',
+                    boxShadow: '0px 0px 3px black',
                 }}>
-                <h4 style={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                }}>
-                Vacuum Cleaner</h4>
+                <h4>{props.name}</h4>
                 <img
                 style={{
                     objectFit: 'contain',
-                    width: '80%',
-                    maxHeight: '60%',
-                    marginLeft: 'auto',
-                    marginRight: 'auto',
-                    display: 'block'
+                    minWidth: '20%',
+                    maxWidth: '20%',
+                    minHeight: '70%',
+                    maxHeight: '70%',
                 }} 
-                src='https://images.pexels.com/photos/4107284/pexels-photo-4107284.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'></img>
+                src={props.img}></img>
+                <p style={{
+                    display: 'inline-block'
+                }}>User: {props.user}<br/>Location: {props.location}<br/>Condition: {props.condition}
+                </p>
             </div>
         </Link>
     )
